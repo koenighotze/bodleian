c5daa175f3a36fac5cbeabda43d6fbf8f8ba71b3
David Schmitz <davidschmitz@posteo.de>
2021-09-06 17:44:21 +0200

    Playing with workflow

--- src/main/kotlin/org/koenighotze/library/Books.kt
+++ src/main/kotlin/org/koenighotze/library/Books.kt
@@ -1,8 +1,14 @@
 package org.koenighotze.library
 
 import org.koenighotze.library.model.Book
-import org.springframework.web.bind.annotation.*
-import java.util.*
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
 
 @RestController
 @RequestMapping("/books")
--- src/test/kotlin/org/koenighotze/library/WellKnownBooks.kt
+++ src/test/kotlin/org/koenighotze/library/WellKnownBooks.kt
@@ -3,10 +3,8 @@ package org.koenighotze.library
 import org.koenighotze.library.model.Book
 import java.util.UUID.randomUUID
 
-class WellKnownBooks {
-    companion object {
-        val books = mutableListOf(
-            Book(randomUUID().toString(), "The Lord of The Rings", "J.R.R. Tolkien", "Todo")
-        )
-    }
+object WellKnownBooks {
+    val books = mutableListOf(
+        Book(randomUUID().toString(), "The Lord of The Rings", "J.R.R. Tolkien", "Todo")
+    )
 }
